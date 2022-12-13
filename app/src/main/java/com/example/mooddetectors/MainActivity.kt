180d40bce8ab49fb9a5f6d17b424dc3ff2962c14
Melvin <melvin.bulan000@gmail.com>
2022-12-13 21:56:35 +0800

    secondCommit

--- app/src/main/java/com/example/mooddetectors/MainActivity.kt
+++ app/src/main/java/com/example/mooddetectors/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.mooddetectors
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
