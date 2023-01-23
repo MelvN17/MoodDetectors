60b9f8d84c8c368e39319ab6a36dadae1125ee44
josephus <josephusmabanag2@gmail.com>
2023-01-23 21:22:26 +0800

    Tflite Model Integrated

--- app/src/main/java/com/example/mooddetectors/CameraActivity.kt
+++ app/src/main/java/com/example/mooddetectors/CameraActivity.kt
@@ -2,24 +2,27 @@ package com.example.mooddetectors
 
 import android.content.Context
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
 import android.view.SurfaceView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import org.opencv.android.BaseLoaderCallback
-import org.opencv.android.CameraBridgeViewBase
+import org.opencv.android.*
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2
-import org.opencv.android.JavaCameraView
-import org.opencv.android.LoaderCallbackInterface
-import org.opencv.android.OpenCVLoader
 import org.opencv.core.*
 import org.opencv.imgproc.Imgproc
 import org.opencv.objdetect.CascadeClassifier
+import org.tensorflow.lite.Interpreter
 import java.io.File
+import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.InputStream
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.channels.FileChannel
+
 
 class CameraActivity : AppCompatActivity(), CvCameraViewListener2 {
     companion object {
@@ -27,59 +30,117 @@ class CameraActivity : AppCompatActivity(), CvCameraViewListener2 {
         const val SCREEN_HEIGHT = 1080
     }
     var cascadefile: File? = null
+    var model: ByteBuffer? = null
+    var interpreter: Interpreter? = null
     //var face_haar_cascade = CascadeClassifier("haarcascade_frontalface_default.xml")
     var faceDetector: CascadeClassifier? = null
-    var javaCameraView: JavaCameraView? = null
     var cameraBridgeViewBase: CameraBridgeViewBase? = null
     var quit: Boolean = false
     var mRGBA: Mat? = null
     var mGrey: Mat? = null
+
+
+    //FPS log variables
+    private var frameCount = 0
+    private var lastLogTime = System.currentTimeMillis()
+
+//    private val loader = object : BaseLoaderCallback(this) {
+//        override fun onManagerConnected(status: Int) {
+//            when (status) {
+//                LoaderCallbackInterface.SUCCESS -> {
+//                    var IS: InputStream = resources.openRawResource(R.raw.haarcascade_frontalface_default)
+//                    var cascadeDir: File = getDir("cascade", Context.MODE_PRIVATE)
+//                    cascadefile = File(cascadeDir, "haarcascade_frontalface_dafault.xml")
+//
+//                    var fos: FileOutputStream = FileOutputStream(cascadefile)
+//
+//                    var buffer = ByteArray(4096)
+//                    var bytesRead: Int = IS.read(buffer)
+//
+//                   while (bytesRead != -1){
+//                       fos.write(buffer, 0, bytesRead)
+//                       bytesRead = IS.read(buffer)
+//
+//                   }
+//
+//                    IS.close()
+//                    fos.close()
+//
+//                    faceDetector = CascadeClassifier(cascadefile?.absolutePath)
+//
+//                    if(faceDetector!!.empty()){
+//                        faceDetector = null
+//                    }else{
+//                        cascadeDir.delete()
+//                    }
+//
+//                    System.loadLibrary("opencv_java4")
+//                    cameraBridgeViewBase?.enableView()
+//                }
+//                else -> super.onManagerConnected(status)
+//            }
+//        }
+//    }
+
+    //With this, we do not need to close the InputStream and FileOutputStream explicitly, the resources will be closed automatically when the try block is exited. This can help to avoid resource leaks and make your code easier to read and maintain.
     private val loader = object : BaseLoaderCallback(this) {
         override fun onManagerConnected(status: Int) {
             when (status) {
                 LoaderCallbackInterface.SUCCESS -> {
-                    var IS: InputStream = resources.openRawResource(R.raw.haarcascade_frontalface_default)
-                    var cascadeDir: File = getDir("cascade", Context.MODE_PRIVATE)
-                    cascadefile = File(cascadeDir, "haarcascade_frontalface_dafault.xml")
+                    try {
+                        val IS: InputStream = resources.openRawResource(R.raw.haarcascade_frontalface_default)
+                        val cascadeDir: File = getDir("cascade", Context.MODE_PRIVATE)
+                        cascadefile = File(cascadeDir, "haarcascade_frontalface_alt_tree.xml")
+                        Log.d("onManagerConnected", "Cascade file loaded successfully")
+                        val fos: FileOutputStream = FileOutputStream(cascadefile)
+                        val buffer = ByteArray(4096)
+                        var bytesRead: Int = IS.read(buffer)
+                        while (bytesRead != -1) {
+                            fos.write(buffer, 0, bytesRead)
+                            bytesRead = IS.read(buffer)
+                        }
+                        faceDetector = CascadeClassifier(cascadefile?.absolutePath)
+                        if (faceDetector!!.empty()) {
+                            faceDetector = null
+                        } else {
+                            cascadeDir.delete()
+                        }
+                        System.loadLibrary("opencv_java4")
+                        cameraBridgeViewBase?.enableView()
 
-                    var fos: FileOutputStream = FileOutputStream(cascadefile)
-
-                    var buffer = ByteArray(4096)
-                    var bytesRead: Int = IS.read(buffer)
-
-                   while (bytesRead != -1){
-                       fos.write(buffer, 0, bytesRead)
-                       bytesRead = IS.read(buffer)
-
-                   }
-
-                    IS.close()
-                    fos.close()
-
-                    faceDetector = CascadeClassifier(cascadefile?.absolutePath)
-
-                    if(faceDetector!!.empty()){
-                        faceDetector = null
-                    }else{
-                        cascadeDir.delete()
+                        //load model
+                        val fileDescriptor = assets.openFd("model.tflite")
+                        val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
+                        val fileChannel = inputStream.channel
+                        val startOffset = fileDescriptor.startOffset
+                        val declaredLength = fileDescriptor.declaredLength
+                        model = fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
+                        interpreter = Interpreter(model!!)
+                        Log.d("onManagerConnected", "Cascade file loaded successfully")
+                    } catch (e: Exception) {
+                        // handle exception here
+                        Log.e("onManagerConnected", "Error loading cascade file: ${e.message}")
+                        Toast.makeText(this@CameraActivity, "Error loading cascade file", Toast.LENGTH_SHORT).show()
                     }
-
-                    System.loadLibrary("opencv_java4")
-                    cameraBridgeViewBase?.enableView()
                 }
                 else -> super.onManagerConnected(status)
             }
         }
     }
 
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_camera)
+        setContentView(R.layout.activity_camera).apply{
+            cameraBridgeViewBase = findViewById(R.id.cameraView)
+        }
 
 
 
-        cameraBridgeViewBase = findViewById(R.id.cameraView)
-//        javaCameraView = findViewById(R.id.cameraView)
+
         getPermission()
         cameraBridgeViewBase?.setCvCameraViewListener(this)
 
@@ -91,9 +152,7 @@ class CameraActivity : AppCompatActivity(), CvCameraViewListener2 {
 //        initView()
 //        initListener()
 
-//        while (!quit) {
-//
-//        }
+
 
     }
 
@@ -140,7 +199,7 @@ class CameraActivity : AppCompatActivity(), CvCameraViewListener2 {
 
     override fun onCameraViewStarted(width: Int, height: Int) {
         mRGBA = Mat()
-        mGrey = Mat()
+        mGrey = Mat(height, width, CvType.CV_8UC1)
 //        frame = Mat(height, width, CvType.CV_8UC4)
 //        cameraBridgeViewBase!!.enableFpsMeter()
 //        dnnHelper.onCameraViewStarted(this)
@@ -155,25 +214,170 @@ class CameraActivity : AppCompatActivity(), CvCameraViewListener2 {
 
     override fun onCameraFrame(inputFrame: CameraBridgeViewBase.CvCameraViewFrame): Mat {
         mRGBA = inputFrame.rgba()
-//        frameT = frame!!.t()
-//        Core.flip(frame!!.t(), frameT, 1)
-//        Imgproc.resize(frameT, frameT, frame!!.size())
 
         mGrey = inputFrame.gray()
-
-        //detect faces
+        Core.flip(mRGBA!!.t(),mRGBA,1);
+        Core.flip(mGrey!!.t(),mGrey,1);
+        //detect faces and predict
         var faceDetections: MatOfRect = MatOfRect()
-        faceDetector!!.detectMultiScale(mRGBA, faceDetections)
+        faceDetector!!.detectMultiScale(mGrey, faceDetections)
 
-        for(rect:Rect in faceDetections.toArray()){
-            Imgproc.rectangle(mRGBA, Point(rect.x.toDouble(), rect.y.toDouble()), Point((rect.x + rect.width).toDouble(), (rect.y + rect.height).toDouble()), Scalar(255.0, 255.0,0.0))
+        try {
+            for (rect:Rect in faceDetections.toArray()) {
+                Imgproc.rectangle(mRGBA, Point(rect.x.toDouble(), rect.y.toDouble()), Point((rect.x + rect.width).toDouble(), (rect.y + rect.height).toDouble()), Scalar(255.0, 0.0, 0.0), 2)
+                val roiGray = mGrey!!.submat(rect.y - 5, rect.y + rect.height + 5, rect.x - 5, rect.x + rect.width + 5)
+//                Imgproc.resize(roiGray, roiGray, Size(48.0, 48.0))
+                val imagePixels = roiGray
+                val bmp = Bitmap.createBitmap(imagePixels.cols(), imagePixels.rows(), Bitmap.Config.ARGB_8888)
+
+                Utils.matToBitmap(imagePixels, bmp)
+                val scaledBitmap = Bitmap.createScaledBitmap(bmp, 48, 48, false)
+                //convert bitmap to byte array*
+                val byteBuffer = ByteBuffer.allocateDirect(4*1*48*48*1)
+                byteBuffer.order(ByteOrder.nativeOrder())
+                val intValue = IntArray(48*48)
+                scaledBitmap.getPixels(intValue, 0, scaledBitmap.width, 0, 0, scaledBitmap.width,scaledBitmap.height)
+                var pixel = 0
+                for (i in 0 until 48) {
+                    for (j in 0 until 48) {
+                        val value: Int = intValue[pixel++]
+                        //normalize image
+//                        byteBuffer.putFloat((value shr 16 and 0xFF) / 255.0f)
+//                        byteBuffer.putFloat((value shr 8 and 0xFF) / 255.0f)
+                        byteBuffer.putFloat((value and 0xFF) / 255.0f)
+                    }
+                }
+
+//                    bmp.copyPixelsToBuffer(byteBuffer)
+//                    val byteArray = byteBuffer.array()
+//
+////pass byte array to the TensorFlow Lite interpreter
+//                    val roiGreyUint8 = Mat()
+//                    Core.normalize(roiGray, roiGreyUint8, 0.0, 255.0, Core.NORM_MINMAX, CvType.CV_8UC1)
+//                    val input = Array(1) { ByteArray(48 * 48) }
+//                    roiGreyUint8.get(0, 0, input[0])
+//
+//
+                val output = Array(1) { FloatArray(7) }
+                interpreter!!.run(byteBuffer, output)
+                val maxIndex = output[0].indices.maxBy { output[0][it] }!!
+                val emotionLabels = arrayOf("angry", "disgust", "fear", "happy", "sad", "surprise", "neutral")
+                val emotion = emotionLabels[maxIndex]
+                Imgproc.putText(mRGBA, "Sentiment: $emotion", Point(rect.x.toDouble(), (rect.y + rect.height + 40).toDouble()), Imgproc.FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255.0, 255.0, 0.0))
+
+            }
+        } catch (e: Exception) {
+            Log.e("ERRORRR: ", e.stackTraceToString())
         }
-        var frameT = mRGBA!!.t()
-        Core.flip(mRGBA!!.t(), frameT, 1);
+
+
+//        try {
+//            for(rect:Rect in faceDetections.toArray()){
+//                Imgproc.rectangle(mRGBA, Point(rect.x.toDouble(), rect.y.toDouble()), Point((rect.x + rect.width).toDouble(), (rect.y + rect.height).toDouble()), Scalar(255.0, 255.0,0.0))
+//                // crop the face detected and resize to 48 x 48(the input of the model)
+//                var roiGrey = mGrey!!.submat(rect.y - 5, rect.y + rect.height + 5, rect.x - 5, rect.x + rect.width + 5)
+//                Imgproc.resize(roiGrey, roiGrey, Size(48.0, 48.0))
+//                //converting the image to bitmaps
+//                var imagePixels = roiGrey
+//
+//                if(!imagePixels.empty()){
+//                    val bmp = Bitmap.createBitmap(imagePixels.cols(), imagePixels.rows(), Bitmap.Config.ARGB_8888)
+//                    Utils.matToBitmap(imagePixels, bmp)
+//
+//                    //convert bitmap to byte array
+//                    val byteBuffer = ByteBuffer.allocate(bmp.byteCount)
+//                    bmp.copyPixelsToBuffer(byteBuffer)
+////                    val byteArray = byteBuffer.array()
+////
+////                    //pass byte array to the TensorFlow Lite interpreter
+////                    val roiGreyUint8 = Mat()
+////                    Core.normalize(roiGrey, roiGreyUint8, 0.0, 255.0, Core.NORM_MINMAX, CvType.CV_8UC1)
+////                    val input = Array(1) { ByteArray(48 * 48) }
+////                    roiGreyUint8.get(0, 0, input[0])
+////
+////                    var output = Array(1) { FloatArray(7) }
+////                    interpreter?.run(input, output)
+//
+//                    //pass byte buffer to the TensorFlow Lite interpreter
+//                    val input = Array(1) { ByteArray(48 * 48) }
+//                    val roiGreyUint8 = Mat()
+//                    Core.normalize(roiGrey, roiGreyUint8, 0.0, 255.0, Core.NORM_MINMAX, CvType.CV_8UC1)
+//                    roiGreyUint8.get(0, 0, input[0])
+//                    val output = Array(1) { IntArray(7) }
+//                    interpreter?.run(input, output)
+//
+//                    //get the index of the highest prediction
+//                    val maxIndex = output[0].indices.maxBy { output[0][it] }!!
+//
+//                    // Map the index to the corresponding emotion
+//                    val emotionLabels = arrayOf("angry", "disgust", "fear", "happy", "sad", "surprise", "neutral")
+//                    val emotion = emotionLabels[maxIndex]
+//
+//                    //display the emotion prediction on the screen
+//                    Imgproc.putText(mRGBA, "Sentiment: $emotion", Point(rect.x.toDouble(), (rect.y + rect.height + 40).toDouble()), Imgproc.FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255.0, 255.0, 0.0))
+////                    Toast.makeText(this@CameraActivity, "Sentiment: $emotion", Toast.LENGTH_SHORT).show()
+//
+//
+//                }else{
+//                    Log.d("EMPTY", "EMPTY")
+//                }
+//
+//            }
+//
+//        }catch(e: Exception){
+//            Log.e("ERRORRR: ", ""+ frameCount + " " + e.message.toString());
+//        }
+
+        //var frameT = mRGBA!!.t()
+//        Core.flip(mRGBA!!.t(),mRGBA,0);
+
+
+        //FPS LOGGING
+        frameCount++
+        logFPS()
 
 
 
-        return frameT
+        return mRGBA!!
+    }
+
+//    //optimized version
+//    override fun onCameraFrame(inputFrame: CameraBridgeViewBase.CvCameraViewFrame): Mat {
+//        //Convert the RGBA image to grayscale for faster processing
+//        mGrey = inputFrame.gray()
+//        val inputFrameSize = inputFrame.rgba().size()
+//        val frameWidth = inputFrameSize.width
+//        val frameHeight = inputFrameSize.height
+//        //Resize the image to reduce processing time
+//        Imgproc.resize(mGrey, mGrey, Size(frameWidth/2, frameHeight/2))
+//
+//
+//
+//        //detect faces using grayscale image and smaller scale factor
+//        var faceDetections: MatOfRect = MatOfRect()
+//        faceDetector!!.detectMultiScale(mGrey, faceDetections, 1.1, 3, 0, Size(30.0, 30.0))
+//
+//
+//
+//        //Draw rectangles on the faces
+//        for(rect:Rect in faceDetections.toArray()){
+//            Imgproc.rectangle(mGrey, Point(rect.x.toDouble(), rect.y.toDouble()), Point((rect.x + rect.width).toDouble(), (rect.y + rect.height).toDouble()), Scalar(255.0, 255.0,0.0))
+//        }
+//
+//
+//
+//        Imgproc.resize(mGrey, mGrey, Size(frameWidth, frameHeight))
+//        //Convert grayscale image to RGB
+//        mRGBA = Mat()
+//        Imgproc.cvtColor(mGrey, mRGBA, Imgproc.COLOR_GRAY2RGBA)
+//
+//        //FPS LOGGING
+//        frameCount++
+//        logFPS()
+//
+//        return mRGBA!!
+//    }
+
 //        mCurrentFrame = inputFrame.rgba()
 //        val image = mCurrentFrame.clone()
 //        Imgproc.cvtColor(image, image, Imgproc.COLOR_RGB2BGR)
@@ -196,7 +400,18 @@ class CameraActivity : AppCompatActivity(), CvCameraViewListener2 {
 //            setCurrentSpeed()
 //        }
 //        return mCurrentFrame
+
+
+    // logic for logging the FPS
+    private fun logFPS() {
+        val currentTime = System.currentTimeMillis()
+        if (currentTime - lastLogTime >= 1000) {
+            Log.d("FPS", "FPS: " + frameCount)
+            frameCount = 0
+            lastLogTime = currentTime
+        }
     }
+
 
     private fun getPermission() {
         if (ContextCompat.checkSelfPermission(
